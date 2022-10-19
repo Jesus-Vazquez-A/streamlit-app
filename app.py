f28e9fcb4b333e80e628a5c51ff506aba272e195
Amado de Jesús Vázquez Acuñas <85312561+Jesus-Vazquez-A@users.noreply.github.com>
2022-10-18 20:46:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,22 +27,22 @@ st.cache(allow_output_mutation=True)
 
 def input_data():
     
+    region=st.radio("Region",('Southeast', 'Northeast', 'Southwest', 'Northwest'))
+    
     age=st.slider(label="Age",min_value=18,max_value=64,step=1),
     
     sex=st.selectbox("Sex",("Male","Female")),
     
-    bmi=st.number_input(label="BMI",min_value=18.0,max_value=47.0,step=0.0001),
-    
     children=st.slider(label="Children",min_value=0,max_value=5,step=1),
     
-    smoker=st.selectbox("Smoker",("No","Yes")),
+    bmi=st.number_input(label="BMI",min_value=18.0,max_value=47.0,step=0.0001),
     
     medical_problem=st.selectbox("Medical Problem",("Light","Severe")),
     
-    region=st.radio("Region",('Southeast', 'Northeast', 'Southwest', 'Northwest'))
+    smoker=st.selectbox("Smoker",("No","Yes"))
     
     
-    return age,sex,bmi,children,smoker,medical_problem,region
+    return region,age,sex,children,bmi,medical_problem,smoker
 
 
 
